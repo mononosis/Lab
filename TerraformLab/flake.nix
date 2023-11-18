36058480af6a9f8f75a22f1eb0d1fa1ea7dbeba9
Your Name <you@example.com>
2023-11-18 15:43:47 +0000

    customise nvim environment

--- TerraformLab/flake.nix
+++ TerraformLab/flake.nix
@@ -4,16 +4,28 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";  
     flake-utils.url = "github:numtide/flake-utils";
+    terraform-config.url = "github:mononosis/neovim-terraform-lab/main";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: 
+  outputs = { self, nixpkgs, flake-utils, terraform-config, ... }: 
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = [ pkgs.terraform pkgs.neovim pkgs.google-cloud-sdk ];
+          buildInputs = [ 
+            pkgs.terraform 
+            pkgs.google-cloud-sdk 
+            pkgs.neovim
+            (nixpkgs.legacyPackages.x86_64-linux.neovim.override {
+              # Use the custom configuration for Terraform
+              configure = {
+                customRC = builtins.readFile (terraform-config + "/terraform-nvim-config.lua");
+              };
+            })
+
+          ];
         };
       });
 }
