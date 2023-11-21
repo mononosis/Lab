fe5106c4a8dbbca83e04e5c52b9a49cd92442f8e
Your Name <you@example.com>
2023-11-21 12:50:33 +0000

    everything

--- GoLab/flake.nix
+++ GoLab/flake.nix
@@ -14,20 +14,20 @@
           owner = "ray-x";
           repo = "go.nvim";
           rev = "master";
-          sha256 = "";
+          sha256 = "sha256-2eXvIqPJP76kJwnLtgTLCsti0R0Kzpp1w7ov2xZc2D0=";
         };
         guihuaPlugin = pkgs.fetchFromGitHub {
           owner = "ray-x";
           repo = "guihua.lua";
           rev = "master";
-          sha256 = "";
+          sha256 = "sha256-2eXvIqPJP76kJwnLtgTLCsti0R0Kzpp1w7ov2xZc2D0=";
         };
         # Fetch the Lua configuration file directly from GitHub
         goNvimConfig = pkgs.fetchFromGitHub {
           owner = "mononosis";
           repo = "nvim-go-config";
           rev = "main"; # Use the appropriate commit or tag
-          sha256 = ""; # Replace with the correct hash
+          sha256 = "sha256-AmQiaRqiQ4+TxO9XY3Ks/TTszqtN1kz0K6zubq0SN30="; # Replace with the correct hash
         };
       in
       {
