ad8fa745e3edc88bbdea483d972de7438152e9ea
Your Name <you@example.com>
2023-11-25 13:53:36 +0000

    test

--- JSPlusLab/flake.nix
+++ JSPlusLab/flake.nix
@@ -10,28 +10,33 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        seoulVimColor = pkgs.fetchFromGitHub {
-          owner = "junegunn";
-          repo = "seoul256.vim";
-          rev = "master";
-          sha256 = "sha256-PHplqWYT9sxU/Z/U+bRfCqiCOITjTp1T3xytZBBgQy4=";
-        };
         materialVimColor = pkgs.fetchFromGitHub {
-          owner = "jdkanani";
-          repo = "vim-material-theme";
+          owner = "Rigellute";
+          repo = "shades-of-purple.vim";
           rev = "master";
-          sha256 = "sha256-2y4G2mqMrArg+bMI3xJsv6SzELSuO64JlzjjHIX/uz8=";
+          sha256 = "sha256-iiGASgVlIXnnUNBlp9viKgDBfHiOP5P/yJx9XyELT9g=";
+        };
+        jsPlusNeovimLab = pkgs.fetchFromGitHub {
+          owner = "mononosis";
+          repo = "nvim-jsplus-config";
+          rev = "main"; # Use the appropriate commit or tag
+          sha256 = "sha256-HDMxWQgGhT4k975ysoDQmM9Ye4Aoy08x12j9AQnvhqo="; # Replace with the correct hash
         };
       in
       {
         devShell = pkgs.mkShell {
           buildInputs = [
-            pkgs.nodejs_20
+            pkgs.nodejs_18
             pkgs.nodePackages.typescript-language-server
-
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.pnpm
+            pkgs.yarn
+            pkgs.openssl
           ];
           shellHook = ''
-            export NVIM_PLUGIN_PATHS="${materialVimColor}:${seoulVimColor}"
+            export NODE_OPTIONS=--openssl-legacy-provider
+            export NVIM_PLUGIN_PATHS="${materialVimColor}"
+            export PROJECT_NVIM_CONFIG=${jsPlusNeovimLab}
           '';
         };
       });
