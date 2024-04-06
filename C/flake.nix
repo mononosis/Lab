e08e86f9191fe8e2e962bf543475d032a7867f1a
Your Name <you@example.com>
2024-04-06 06:15:05 +0000

    test

---
+++ C/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "C and its ecosystem projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            wrapGAppsHook
+          ];
+          buildInputs = with pkgs; [
+            wayland
+            gcc
+
+          ];
+        };
+      });
+}
