c1c6556a2b42b2474b764a0844e8be44a8298f62
Your Name <you@example.com>
2024-06-07 00:37:56 +0000

    test

---
+++ RubyLab/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Groovy projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nvimProjectLab = pkgs.fetchFromGitHub {
+          name = "nvim-groovy-config";
+          owner = "mononosis";
+          repo = "nvim-groovy-config";
+          rev = "main"; 
+          sha256 = "sha256-XtrlLOvSP8Kt6JblA1JaV6ngDd8l1vwKWC55ESHu0FM="; 
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.bundler
+          ];
+          shellHook = ''
+            export NVIM_PLUGIN_PATHS=""
+            [[ ! -z $NIX_DEV_MODE ]] \
+                && echo "We are in dev mode" \
+                && export PROJECT_NVIM_CONFIG=$HOME/Lab/LuaLab/${nvimProjectLab.repo} \
+                || export PROJECT_NVIM_CONFIG=${nvimProjectLab}
+          '';
+        };
+      });
+}
