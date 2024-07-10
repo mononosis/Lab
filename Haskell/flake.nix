abab3bfd0be47311923751c9812cf34cab74ee29
Your Name <you@example.com>
2024-07-10 04:54:20 +0000

    quick push

---
+++ Haskell/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Lua projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            
+            pkgs.haskell.compiler.ghc981
+          ];
+        };
+      });
+}
